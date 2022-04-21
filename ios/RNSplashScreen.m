fe1bf2f9862f6b7b43e9d20b3196545820b670c1
hao <unnamed.hao@gmail.com>
2022-04-21 15:23:36 +0800

    change

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -34,7 +34,7 @@ RCT_EXPORT_MODULE(SplashScreen)
 
 + (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView {
     if (!loadingView) {
-        loadingView = [[[NSBundle mainBundle] loadNibNamed:splashScreen owner:self options:nil] objectAtIndex:0];
+        loadingView = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
         CGRect frame = rootView.frame;
         frame.origin = CGPointMake(0, 0);
         loadingView.frame = frame;
